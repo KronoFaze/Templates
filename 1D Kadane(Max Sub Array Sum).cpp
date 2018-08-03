39c65c8ee20825c44ab96ad946a762165773ca58
Arifur Rahman Rabbi <rabbiarifurrahman@gmail.com>
2018-08-04 01:33:16 +0600

    Update 1D Kadane(Max Sub Array Sum).cpp

--- 1D Kadane(Max Sub Array Sum).cpp
+++ 1D Kadane(Max Sub Array Sum).cpp
@@ -1,10 +1,9 @@
 int maxSubArraySum(vector <int> &v) {
-    int localMax = 0;
+    int localMax = v[0];
     int globalMax = v[0];
-    for(int i = 0; i < v.size(); ++i) {
-        localMax += v[i];
+    for(int i = 1; i < v.size(); ++i) {
+        localMax = max(v[i], localMax + v[i]);
         globalMax = max(globalMax, localMax);
-        if(localMax < 0) localMax = 0;
     }
     return globalMax;
 }
