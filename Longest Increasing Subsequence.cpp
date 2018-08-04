5a274b87c97487c80dd829244ace6dcc3d47074e
Arifur Rahman Rabbi <rabbiarifurrahman@gmail.com>
2018-08-04 13:54:07 +0600

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -1,25 +1,25 @@
 vector <int> lis(vector <int> &v) {
-    vector <int> lisVec;
-    vector <int> ans;
+    vector <int> lisV;
+    vector <int> ret;
     vector <int> sol(v.size(), -1);
-    for(int i = 0; i < v.size(); ++i) {
-        int pos = lower_bound(lisVec.begin(), lisVec.end(), i, [&](int x, int y) { return v[x] < v[y]; }) - lisVec.begin(); //strictly increasing
-        //int pos = lower_bound(lisVec.begin(), lisVec.end(), i, [&](int x, int y) { return v[x] <= v[y]; }) - lisVec.begin(); //non-decreasing
-        //int pos = lower_bound(lisVec.begin(), lisVec.end(), i, [&](int x, int y) { return v[x] > v[y]; }) - lisVec.begin(); //strictly decreasing
-        //int pos = lower_bound(lisVec.begin(), lisVec.end(), i, [&](int x, int y) { return v[x] >= v[y]; }) - lisVec.begin(); //non-increasing
-        if(pos == lisVec.size()) {
-            if(lisVec.size() > 0) sol[i] = lisVec.back();
-            lisVec.push_back(i);
+    auto cmp = [&](int x, int y) { return v[x] > v[y]; };   //  '>' for strictly increasing
+                                                            //  '>=' for non-decreasing
+                                                            //  '<' for strictly decreasing
+                                                            //  '<=' for non-increasing
+    for(int i = v.size() - 1; i >= 0; --i) {
+        int pos = lower_bound(lisV.begin(), lisV.end(), i, cmp) - lisV.begin();
+        if(pos == lisV.size()) {
+            if(pos > 0) sol[i] = lisV.back();
+            lisV.push_back(i);
         } else {
-            if(pos > 0) sol[i] = lisVec[pos - 1];
-            lisVec[pos] = i;
+            if(pos > 0) sol[i] = lisV[pos - 1];
+            lisV[pos] = i;
         }
     }
-    int idx = lisVec.back();
+    int idx = lisV.back();
     while(idx != -1) {
-        ans.push_back(v[idx]);
+        ret.push_back(v[idx]);
         idx = sol[idx];
     }
-    reverse(ans.begin(), ans.end());
-    return ans;
+    return ret;
 }
